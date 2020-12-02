c81fd7e9298f5cb63e262eeed9d346089ba476d0
lisabost <lbost@my.wctc.edu>
2020-12-02 15:35:39 -0600

    updated gradebook to store student grades as objects in an array added buttons to sort by name and sort by percentage grade

--- code.js
+++ code.js
@@ -1,45 +1,106 @@
 $(document).ready(function () {
 
-
         $("form#gradebook").submit(submitGrade);
+        $("#nameSort").click(sortByName);
+        $("#percentSort").click(sortByPercent)
+
+
+        var gradeBook = [];
 
         //event handlers
 
         function submitGrade(event) {
             event.preventDefault();
 
-            //make a grade object to hold our form data
-            var grade = {
-                firstName: $("#firstName").val(),
-                lastName: $("#lastName").val(),
-                pointsEarned: $("#pointsEarned").val(),
-                pointsPossible: $("#pointsPossible").val()
-            }
+            //get the form data
+            var firstName = $("#firstName").val();
+            var lastName = $("#lastName").val();
+            var pointsEarned = $("#pointsEarned").val();
+            var pointsPossible = $("#pointsPossible").val();
+
+            var fullName = `${firstName} ${lastName}`;
+
 
             //calculate the percentage
-            var percentage = grade.pointsEarned / grade.pointsPossible * 100;
-            var percentageDisplay = percentage.toFixed(1) + "%";
+            var percentage = pointsEarned / pointsPossible * 100;
+            var percentageDisplay = percentage.toFixed(1);
             var letterGrade = "";
 
             //find the letter grade
-            if(percentage > 90) {
+            if (percentage > 90) {
                 letterGrade = "A";
-            } else if(percentage > 80) {
+            } else if (percentage > 80) {
                 letterGrade = "B";
-            } else if(percentage > 70) {
+            } else if (percentage > 70) {
                 letterGrade = "C";
-            } else if(percentage > 60) {
+            } else if (percentage > 60) {
                 letterGrade = "D";
             } else {
                 letterGrade = "F";
             }
 
-            //display the results
-            $("p#message").html(`Student: ${grade.firstName} ${grade.lastName}<br>
-            Percentage points: ${percentageDisplay}<br>Letter Grade: ${letterGrade}`);
+            //make a student grade object to hold our name, percentage, and letter grade
+            var studentGrade = {
+                name: fullName,
+                percentage: percentage,
+                letterGrade: letterGrade
+            }
+
+            //instead of displaying results, store them in our array
+            gradeBook.push(studentGrade);
+
+            var output = "";
+            //display the contents of our array on the screen
+            for(var i = 0; i < gradeBook.length; i++) {
+                output += "Name: " + gradeBook[i].name + " ";
+                output += "Percentage: " + gradeBook[i].percentage + "% ";
+                output += "Letter Grade: " + gradeBook[i].letterGrade + "<br/>";
+            }
+            $("#message").html(`${output}`);
+
         }
 
-        //other functions
+        function sortByName() {
+            function nameSort(n1, n2) {
+                if(n1.name < n2.name) {
+                    return -1;
+                } else if(n1.name > n2.name) {
+                    return 1;
+                } return 0;
+            }
+            gradeBook.sort(nameSort);
+
+            var output = "";
+            //display the contents of our array on the screen
+            for(var i = 0; i < gradeBook.length; i++) {
+                output += "Name: " + gradeBook[i].name + " ";
+                output += "Percentage: " + gradeBook[i].percentage + "% ";
+                output += "Letter Grade: " + gradeBook[i].letterGrade + "<br/>";
+            }
+            $("#message").html(`${output}`);
+
+        }
+
+        function sortByPercent() {
+            function percentSort(n1, n2) {
+                if(n1.percentage < n2.percentage) {
+                    return -1;
+                } else if(n1.percentage > n2.percentage) {
+                    return 1;
+                } return 0;
+            }
+            gradeBook.sort(percentSort)
+
+            var output = "";
+            //display the contents of our array on the screen
+            for(var i = 0; i < gradeBook.length; i++) {
+                output += "Name: " + gradeBook[i].name + " ";
+                output += "Percentage: " + gradeBook[i].percentage + "% ";
+                output += "Letter Grade: " + gradeBook[i].letterGrade + "<br/>";
+            }
+            $("#message").html(`${output}`);
+        }
+
 
     }
 );
